71f9038ad37f771aad9a1adcbdeb906dc14bc28c
ObserverTechC <ovsaba0721@gmail.com>
2017-01-20 15:10:21 +0900

    WeAreTech.C向けの更新
    コメントの追加

--- Script/Chat.cs
+++ Script/Chat.cs
@@ -8,29 +8,32 @@ public class Chat : MonoBehaviour {
     [SerializeField] RectTransform content;
 
     //生成する要素
-    [SerializeField] RectTransform chatBack;
+    [SerializeField] RectTransform chatBack; //チャットの背景画像
 
-    [SerializeField] Text chatText;
+    [SerializeField] Text chatText; //チャットのテキスト
 
-    public string[] talk;
-    public int currenttalk = 0;
+    public string[] talk; //チャットの内容を格納
+    public int currenttalk = 0;// 現在のテキスト
     public stickset st;
-    Image chatcolor;
-    public Image haikei;
+    Image chatcolor; //チャットの枠の色
+    public Image haikei; //チャット背景
     public GameObject scroll;
     public jairon Jairon;
-	public GameObject[] obj; 
+    public GameObject[] obj;
+
     void Awake()
     {
-        chatBack.gameObject.SetActive(false);
-        haikei.enabled = false;
+
+        chatBack.gameObject.SetActive(false); 
+        haikei.enabled = false; 
         scroll.SetActive(false);
     }
 
     void Start()
     {
-        this.GetComponent<Canvas>().enabled = true;
-        st.enabled = false;
+
+        this.GetComponent<Canvas>().enabled = true; 
+        st.enabled = false; //sticksetを無効
         haikei.enabled = true;
         scroll.SetActive(true);
         chatcolor = chatBack.GetComponent<Image>();
@@ -42,28 +45,36 @@ public class Chat : MonoBehaviour {
         
        if (Input.GetButtonDown("Fire1"))
         {
+           //currenttalkが９以上で処理
             if (currenttalk >= 9)
             {
                 haikei.enabled = false;
                 scroll.SetActive(false);
-                st.enabled = true;
+                st.enabled = true; //sticksetを有効
                 this.GetComponent<Chat>().enabled = false;
                 Jairon.enabled = true;
-				for (int i = 0; i < obj.Length; i++) {
-					obj [i].SetActive (false);
-				}
+
+                for (int i = 0; i < obj.Length; i++)
+                {
+                    obj[i].SetActive(false);
+                }
+
             }
             else
             {
                 OnSubmit();
+                
             }
             
         }
       
+      
     }
+    
     
     public void OnSubmit()
     {
+        //currenttalkによってチャットの枠の色を変える
         switch (currenttalk)
         {
             case 0:
@@ -71,13 +82,13 @@ public class Chat : MonoBehaviour {
             case 3:
             case 5:
             case 7:
-                chatcolor.color = new Color(1.0f, 1.0f, 1.0f);
+                chatcolor.color = new Color(1.0f, 1.0f, 1.0f); //白
                 break;
             case 1:
             case 4:
             case 6:
             case 8:
-                chatcolor.color = new Color(0.0f, 1.0f, 0.3f);
+                chatcolor.color = new Color(0.0f, 1.0f, 0.3f); //緑
                 break;
 
             default:
@@ -86,18 +97,17 @@ public class Chat : MonoBehaviour {
 
 
         }
-        // 入力フィールドを元に複製元のデータを改変
+        // textを切り替える
         chatText.text = talk[currenttalk];
         currenttalk++;
        
         
         //content以下にchatBackを複製
         var element = GameObject.Instantiate<RectTransform>(chatBack);
-        
         element.SetParent(content, false);
         element.SetAsLastSibling();
         element.gameObject.SetActive(true);
-        
+
         
     }
 }
